d7030dc7dacb500976af2647db1f5e6e5b5db1aa
Arleidis C <arlcornd@gmail.com>
2025-10-03 18:37:11 -0500

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+<head>
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<?php
+// footer.php
+?>
+<footer class="footer">
+  <div class="footer-container">
+    <div class="footer-info">
+      <h3 class="footer-title">La Fragata Giratoria</h3>
+      <p class="footer-subtitle">Descubre el Sabor del Mar en <strong>Bogotá</strong></p>
+      <p class="footer-contact">Restaurante La Fragata Giratoria<br />fragatagiratoria@gmail.com</p>
+      <div class="footer-redes">
+        <a href="#" aria-label="Facebook de La Fragata Giratoria"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/lafragatagiratorio/?hl=es" target="_blank" rel="noopener noreferrer" aria-label="Instagram de La Fragata Giratoria">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </div>
+    </div>
+    <div class="footer-policies">
+      <a href="#" class="policy-link">Política de Privacidad</a>
+      <a href="#" class="policy-link">Declaración de Accesibilidad</a>
+      <a href="#" class="policy-link">Términos y Condiciones</a>
+      <a href="#" class="policy-link">Política de Reembolso</a>
+    </div>
+  </div>
+</footer>
