1341265f49b341d3be2874f447d601bef40f06bc
Arleidis C <arlcornd@gmail.com>
2025-10-03 17:42:06 -0500

    Create contacto.blade.php

---
+++ resources/views/emails/contacto.blade.php
@@ -0,0 +1,6 @@
+<h1>Nuevo mensaje de contacto</h1>
+
+<p><strong>Nombre:</strong> {{ $data['nombre'] }}</p>
+<p><strong>Email:</strong> {{ $data['email'] }}</p>
+<p><strong>Asunto:</strong> {{ $data['asunto'] }}</p>
+<p><strong>Mensaje:</strong> {{ $data['mensaje'] }}</p>
